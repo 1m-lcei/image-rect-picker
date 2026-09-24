550b05eba811d5efd5a07f9409d5a78eb089fd6e
1m-lcei <187761927+1m-lcei@users.noreply.github.com>
2026-09-24 22:01:54 +0900

    build: add vite plugins to strip html comments

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,15 @@ import { defineConfig } from "vite";
 export default defineConfig({
   base: "./",
   build: { modulePreload: { polyfill: false } },
+  plugins: [
+    {
+      name: "strip-html-comments",
+      apply: "build",
+      transformIndexHtml(html) {
+        return html
+          .replace(/<!--[\s\S]*?-->/g, "")
+          .replace(/^[ \t]*\r?\n/gm, "");
+      },
+    },
+  ],
 });
